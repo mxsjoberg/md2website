31daa54e3e2d386e7912e8a948275fad9d3d382a
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-08-03 00:36:03 +0200

    Update build.py

--- build.py
+++ build.py
@@ -6,6 +6,9 @@ from datetime import datetime
 
 ROOT_DIR = "dist"
 
+def sort_by_date_and_title(item):
+    return (item["date"], item["title"])
+
 def write_header(file, title="Built with Static Webpage Builder", root=0):
     file.write("<html lang='en'>")
     file.write("<head>")
@@ -83,8 +86,6 @@ with open(f"{ROOT_DIR}/posts.html", "w+") as html_posts:
                     write_footer(tmp_file)
                     post_file.close()
     # sort posts_lst by date then by name
-    def sort_by_date_and_title(item):
-        return (item["date"], item["title"])
     sorted_posts_lst = sorted(posts_lst, key=sort_by_date_and_title, reverse=True)
     # write posts
     current_date = None
@@ -94,6 +95,40 @@ with open(f"{ROOT_DIR}/posts.html", "w+") as html_posts:
             current_date = post['date'].year
         html_posts.write(f"<p><a href='{post['url']}.html'>{post['title']}</a></p>")
     write_footer(html_posts)
+
+# programming
+with open(f"{ROOT_DIR}/programming.html", "w+") as html_programming:
+    write_header(html_programming, title="Programming")
+    posts_lst = []
+    for root, dirs, posts in os.walk("programming"):
+        for post in posts:
+            if post.split(".")[1] == "md":
+                post_name = post.split(".")[0]
+                # create page
+                with open(f"{ROOT_DIR}/{post_name}.html", "w+") as tmp_file:
+                    post_file = open(f"{root}/{post}")
+                    post_content = post_file.read()
+                    # title (first line)
+                    title = post_content.split("\n")[0].split("# ")[1]
+                    # date (third line)
+                    date = post_content.split("\n")[2].split("*")[1]
+                    # append to posts_lst
+                    posts_lst.append({ "title": title, "date": datetime.strptime(date, "%B %Y"), "url": post_name })
+                    # write
+                    write_header(tmp_file, title=title)
+                    tmp_file.write(markdown.markdown(post_content, extensions=["fenced_code", "tables"]))
+                    write_footer(tmp_file)
+                    post_file.close()
+    # sort posts_lst by date then by name
+    sorted_posts_lst = sorted(posts_lst, key=sort_by_date_and_title, reverse=True)
+    # write posts
+    current_date = None
+    for post in sorted_posts_lst:
+        if current_date != post['date'].year:
+            html_programming.write(f"<p>{post['date'].year}</p>")
+            current_date = post['date'].year
+        html_programming.write(f"<p><a href='{post['url']}.html'>{post['title']}</a></p>")
+    write_footer(html_programming)
 
 # minimize css
 with open(f"{ROOT_DIR}/main.min.css", "w+") as file:
