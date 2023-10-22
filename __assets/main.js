69e220234161ab66dd4de442d7737f44b6b6f642
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-22 20:27:01 +0200

    assets

---
+++ __assets/main.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // toggle theme
+    var theme = document.querySelector("#theme");
+    theme.addEventListener("click", function() {
+        document.querySelector("html").classList.toggle("light");
+        // save to local storage
+        if (document.querySelector("html").classList.contains("light")) {
+            localStorage.setItem("light", "true");
+        } else {
+            localStorage.setItem("light", "false");
+        }
+    });
+    // toggle styling
+    var styling = document.querySelector("#styling");
+    styling.addEventListener("click", function() {
+        document.querySelector("html").classList.toggle("styling");
+        // save to local storage
+        if (document.querySelector("html").classList.contains("styling")) {
+            localStorage.setItem("styling", "true");
+        } else {
+            localStorage.setItem("styling", "false");
+        }
+    });
+    // check local storage
+    if (localStorage.getItem("light") == "true") {
+        document.querySelector("html").classList.add("light");
+    }
+    if (localStorage.getItem("styling") == "true") {
+        document.querySelector("html").classList.add("styling");
+    }
+    // loading time
+    function showPageLoadTime() {
+        // check if the browser supports the performance API
+        if (window.performance && window.performance.timing) {
+            var timing = window.performance.timing;
+            // check if the necessary timing events are available
+            if (timing.loadEventEnd > 0 && timing.navigationStart > 0) {
+                // calculate the DOMContentLoaded time in seconds
+                // var domContentLoadedTime = (timing.domContentLoadedEventEnd - timing.navigationStart) / 1000;
+                var domContentLoadedTime = timing.domContentLoadedEventEnd - timing.navigationStart;
+                // calculate the page load time in seconds
+                // var loadTime = (timing.loadEventEnd - timing.navigationStart) / 1000;
+                var loadTime = timing.loadEventEnd - timing.navigationStart;
+                // write to dom_time
+                // document.querySelector("#dom_time").innerHTML = domContentLoadedTime + " seconds";
+                document.querySelector("#dom_time").innerHTML = domContentLoadedTime + " ms";
+                // write to load_time
+                // document.querySelector("#load_time").innerHTML = loadTime + " seconds";
+                document.querySelector("#load_time").innerHTML = loadTime + " ms";
+            } else {
+                setTimeout(showPageLoadTime, 100);
+            }
+        }
+    }
+    // call the function when the page has fully loaded
+    window.addEventListener("load", showPageLoadTime);
+});
