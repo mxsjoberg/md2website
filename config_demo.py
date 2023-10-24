6eff52e6eba046dd88e09ed8624d037588e9f2c6
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-24 18:53:56 +0200

    Update config_demo.py

--- config_demo.py
+++ config_demo.py
@@ -1,8 +1,10 @@
 SOURCE_PATH = "../michaelsjoberg.com/source"
 DIST_PATH = "../michaelsjoberg.com/dist"
+LOAD_FOLDERS = []
 ASSETS = ["main.scss", "main.js"]
 AUTHOR = "Michael Sjöberg"
 DESCRIPTION = "I build software products, learn about stuff in public, and sometimes write about programming, projects, and finance."
+GOOGLE_TAG = False
 # mobile preferences
 APP_NAME = "Michael Sjöberg"
 APP_THEME = "#0E1525"
