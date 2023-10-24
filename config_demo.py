835f4fc5fbabbf84e688efa6976cfa69865dbf13
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-24 17:21:34 +0200

    Create config_demo.py

---
+++ config_demo.py
@@ -0,0 +1,13 @@
+SOURCE_PATH = "../michaelsjoberg.com/source"
+DIST_PATH = "../michaelsjoberg.com/dist"
+ASSETS = ["main.scss", "main.js"]
+AUTHOR = "Michael Sjöberg"
+DESCRIPTION = "I build software products, learn about stuff in public, and sometimes write about programming, projects, and finance."
+# mobile preferences
+APP_NAME = "Michael Sjöberg"
+APP_THEME = "#0E1525"
+# page settings
+POSTS_ON_INDEX = False # TODO: change this to number
+STYLING = True
+DEFAULT_THEME = "dark" # "dark" | "light"
+ALLOW_CHANGE_THEME = True
