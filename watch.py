38ef72b6f408294bddb03018361ff3c45a5a4b58
Michael Sjöberg <mail@michaelsjoeberg.com>
2023-10-24 10:14:28 +0200

    Update watch.py

--- watch.py
+++ watch.py
@@ -8,8 +8,8 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from livereload import Server
 
-DIST_PATH = "../michaelsjoberg.com/dist"
-# DIST_PATH = "__dist"
+# DIST_PATH = "../michaelsjoberg.com/dist"
+DIST_PATH = "__dist"
 DIR_TO_WATCH = "pages"
 COMMAND_TO_RUN = "python3 build.py"
 
